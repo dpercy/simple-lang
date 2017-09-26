65b21fc43d54f83c24fb9f3d045bd40832593a60
David Percy <davetp425@gmail.com>
2017-09-26 02:22:36 -0400

    kanren monad

--- minikanren.hs
+++ minikanren.hs
@@ -18,17 +18,17 @@ For example, I might want to prove (cons x y) = z while knowing z = 7,
 and later try to prove it while knowing z = (cons 1 2).
 So a goal's result depends on its context.
 
-We also need to thread a piece of state through the goal:
-a goal has to allocate new logic variables.
-So we can just use an Int, and feed that through the input and *each* resulting substitution.
-
+Every substitution also contains a "nextfree" variable name,
+guaranteed not to collide with any existing bindings.
 
 -}
 
-type Goal = (Subst, Int) -> [(Subst, Int)]
+newtype Goal a = Goal { unGoal :: Subst -> [(Subst, a)] }
 
-newtype Subst = Subst { unSubst :: [(Int, Term)] }
-              deriving (Show)
+data Subst = Subst { substNextfree :: Int
+                   , substPairs :: [(Int, Term)]
+                   }
+           deriving (Show)
 
 
 -- TODO allow custom data structures instead
@@ -38,27 +38,37 @@ data Term = Var Int
           deriving (Show, Eq)
 
 
-runGoal :: Goal -> [Subst]
-runGoal g = map fst (g startState)
-  where startState = (Subst [], 1)
+runGoal :: Goal Term -> [Term]
+runGoal (Goal g) = map (\(s, v) -> reify v s) $ g (Subst 0 [])
+
+reify :: Term -> Subst -> Term
+reify t s = case walk t s of
+  Lit x -> Lit x
+  Var x -> Var x -- because (walk t s) is not free in s
+  Cons x y -> Cons (reify x s) (reify y s)
 
 -- true always succeeds, binding no variables.
-true :: Goal
-true (s, i) = [(s, i)]
+true :: Goal ()
+true = Goal $ \s -> [(s, ())]
+
+succeed :: a -> Goal a
+succeed v = Goal $ \s -> [(s, v)]
 
 -- false always fails.
-false :: Goal
-false _ = []
+false :: Goal ()
+false = Goal $ \_ -> []
 
 infixr 2 |||
 infixr 3 &&&
 infix 4 ===
+infixl 1 `bind`
+
 
 -- "or" succeeds when either goal succeeds.
 -- It's complete: neither stream of results is "starved",
 -- because results are interleaved.
-(|||) :: Goal -> Goal -> Goal
-(|||) g h start = interleave (g start) (h start)
+(|||) :: Goal a -> Goal a -> Goal a
+(|||) (Goal g) (Goal h) = Goal $ \s -> interleave (g s) (h s)
 
 interleave :: [a] -> [a] -> [a]
 interleave []     ys = ys
@@ -70,39 +80,69 @@ interleave (x:xs) ys = x:(interleave ys xs)
 -- If either fails, "and" fails.
 -- The second goal is only run if the first goal succeeded,
 -- and any variables bound by the first goal are in effect for the second goal.
-(&&&) :: Goal -> Goal -> Goal
-(&&&) g h start =
+(&&&) :: Goal () -> Goal () -> Goal ()
+(&&&) g h = g `bind` \() -> h
+
+-- "bind" chains goals together.
+-- It runs the first goal.
+-- Then whenever that first goal succeeds, it runs the second goal on the result.
+-- But it interleaves the results so that no subgoal is starved.
+bind :: Goal a -> (a -> Goal b) -> Goal b
+bind g h = Goal $ \start ->
   -- gSuccesses is a possibly-infinite stream.
-  let gSuccesses = g start in
+  let gSuccesses = unGoal g start in
   -- resultStreams is a stream-of-streams.
   -- Each element can be infinite (if h succeeds infinite times),
   -- plus the container can be infinite (if g succeeds infinite times).
-  let resultStreams = map h gSuccesses in
-  foldr interleave [] resultStreams
+  let resultStreams = flip map gSuccesses $ \(s, a) -> unGoal (h a) s
+  in foldr interleave [] resultStreams
 
-(===) :: Term -> Term -> Goal
-(===) t u (subst, i) =
+(===) :: Term -> Term -> Goal ()
+(===) t u = Goal $ \subst ->
   case (walk t subst, walk u subst) of
-   (Lit x, Lit y) -> if x == y then [(subst, i)] else []
-   (Var x, u') -> [(extend x u' subst, i)]
-   (t', Var y) -> [(extend y t' subst, i)]
-   (Cons a b, Cons x y) -> (a === x &&& b === y)(subst, i)
+   (Lit x, Lit y) -> if x == y then [(subst, ())] else []
+   (Var x, u') -> [(extend x u' subst, ())]
+   (t', Var y) -> [(extend y t' subst, ())]
+   (Cons a b, Cons x y) -> unGoal  (a === x &&& b === y) subst
    (_, _) -> []
 
 -- walk returns either a non-Var, or a var not bound in subst.
 -- (The result can *contain* bound variables, but can't *be* one.)
 walk :: Term -> Subst -> Term
-walk (Var x) (Subst s) = case lookup x s of
+walk (Var x) s = case lookup x (substPairs s) of
   Nothing -> Var x
-  Just x' -> walk x' (Subst s)
+  Just x' -> walk x' s
 walk t _ = t
 
 -- extend assumes the variable is free in the subst.
 -- it adds a new subst entry.
 -- TODO what if the subst describes infinite values?
 extend :: Int -> Term -> Subst -> Subst
-extend x t (Subst s) = Subst ((x, t) : s)
+extend x t s = s { substPairs = (x, t) : substPairs s }
    
 
-freshVar :: (Term -> Goal) -> Goal
-freshVar cb = \(subst, i) -> (cb (Var i))(subst, i + 1)
+fresh :: Goal Term
+fresh = Goal $ \(Subst i s) -> [(Subst (i + 1) s, Var i)]
+
+
+{-
+Can it make sense for a "Goal" to return a value?
+For example, freshVar could be a goal that always succeeds,
+and returns a fresh variable (a term).
+
+If a Goal returned a value, it would have to return a value each time it succeeds.
+-}
+
+instance Functor Goal where
+  fmap f gv = pure f <*> gv
+
+instance Applicative Goal where
+  pure = succeed
+  (<*>) gf gv = do
+    f <- gf
+    v <- gv
+    return $ f v
+  
+instance Monad Goal where
+  (>>=) = bind
+  return = succeed
