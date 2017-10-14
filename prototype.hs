2ee4494dbfd7b5a0d238b5ff2b73436a78dffbe7
David Percy <davetp425@gmail.com>
2017-10-14 14:15:22 -0400

    Register new modules with testAll

--- prototype.hs
+++ prototype.hs
@@ -7,9 +7,13 @@ import Test.Hspec
 import Model
 import Parser
 import TypeCheck
+import WellFormedTypes
+import CaseCoverage
 
 testAll :: IO ()
 testAll = hspec $ do
   context "testModel" testModel
   context "testParse" testParse
   context "testTypeCheck" testTypeCheck
+  context "testWellFormedTypes" testWellFormedTypes
+  context "testCaseCoverage" testCaseCoverage
