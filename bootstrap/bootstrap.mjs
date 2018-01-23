3158571e5c3f8d0fdb68fe41d0819829343e58ef
David Percy <davetp425@gmail.com>
2018-01-23 01:46:24 -0500

    It bootstraps!

--- bootstrap/bootstrap.mjs
+++ bootstrap/bootstrap.mjs
@@ -2,6 +2,12 @@
 import fs from "fs";
 import * as compiler from "./compiler.mjs";
 
-const source_code = fs.readFileSync("/dev/stdin").toString();
+var source_code = fs.readFileSync("/dev/stdin").toString();
+
+// drop #lang line if present
+if (source_code.startsWith('#lang ')) {
+    source_code = source_code.slice(source_code.indexOf('\n') + 1);
+}
+
 const js_code = compiler.$compile_program(source_code);
-console.log(js_code);
+process.stdout.write(js_code);
--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 set -ex
 
-for name in bool int string list compiler
+modules=(
+    bool
+    int
+    string
+    list
+    compiler
+)
+
+# 1. cross compile the compiler and its deps to JS
+for name in ${modules[@]}
 do
     racket test-boot.rkt < $name.sl > $name.mjs &
 done
-
 wait
+
+# 2. test the compiler-in-JS on some inputs to see if
+#    it has the same behavior as the compiler-in-Racket.
+for name in ${modules[@]}
+do
+    node --experimental-modules bootstrap.mjs < $name.sl > ${name}2.mjs &
+done
+wait
+
+diff $name.mjs ${name}2.mjs
+echo 'yay!'
