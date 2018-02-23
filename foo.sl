daf42f3cb664507b7a39d35af80f13b61721c286
David Percy <davetp425@gmail.com>
2018-02-23 01:31:41 -0500

    structs and matching

--- foo.sl
+++ foo.sl
@@ -16,3 +16,42 @@ f y = 123
 (f
 
  f)
+
+struct Point x y
+(Point 1 2)
+(Point 1)
+Point
+
+match Point 1 2 {
+  v => v
+}
+
+match Point 1 2 {
+  Point x y => y
+}
+
+struct Wrap x
+
+match Wrap 1 {
+  Point x y => y
+  Wrap q => q
+}
+
+match Wrap (Point 1 2) {
+  Wrap (Wrap _) => 9
+  Wrap (Point x y) => x
+}
+
+struct Yolo
+Yolo
+
+match Yolo {
+  Yolo => 12321
+}
+
+match 5 {
+  Yolo => 9
+  Wrap 5 => 99
+  3 => 999
+  5 => 1337
+}
--- sl-mlang.rkt
+++ sl-mlang.rkt
@@ -1,12 +1,13 @@
 #lang racket
 
 (provide (rename-out [sl:#%module-begin #%module-begin]
-                     [sl:#%app #%app]
+                     [sl:parens #%app]
                      )
          #%datum
          )
 
 (require (for-syntax syntax/parse))
+(require racket/splicing)
 
 (define-syntax sl:#%module-begin
   (syntax-parser
@@ -26,12 +27,13 @@
 (define-syntax statement
   (syntax-parser
     #:datum-literals (= struct)
+    [(_ struct name:id field:id ...) #'(sl:struct name field ...)]
     [(_ x:id = expr)  #'(define x expr)]
     [(_ f:id x:id ... = expr)  #'(define (f x ...) expr)]
-    [(_ expr) #'(#%expression expr)]))
+    [(_ expr ...) #'(#%expression (sl:parens expr ...))]))
 
 
-(define-syntax sl:#%app
+(define-syntax sl:parens
   (syntax-parser
     #:datum-literals (|;|)
     ; Filter out all semicolons; they have no significance inside parens.
@@ -39,10 +41,62 @@
         (~seq (~and (~not |;|) expr)
               |;| ...)
         ...)
-     #'(curried-app expr ...)]))
-(define-syntax curried-app
+     #'(parens-no-semicolons expr ...)]))
+(define-syntax parens-no-semicolons
   (syntax-parser
+    #:datum-literals (match)
+    [(_ match form ...) #'(sl:match form ...)]
     ; Unary parens are fine: they're just grouping.
     [(_ x) #'x]
     ; One or more arguments is curried application.
     [(_ f x0 xs ...) #'(curry f x0 xs ...)]))
+
+(define-syntax sl:struct
+  (syntax-parser
+    [(_ name:id)
+     #'(begin
+         (splicing-local [(struct name () #:transparent
+                            #:constructor-name make-it)]
+           (define name (make-it))))]
+    [(_ name:id field:id ...)
+
+     #'(struct name (field ...) #:transparent)]))
+
+(define-syntax sl:match
+  (syntax-parser
+    #:datum-literals (#%braces => |;|)
+    [(_ arg-forms ...
+        {#%braces
+         |;|...
+         (~seq (~and lhs-forms (~not =>) (~not |;| )) ...
+               =>
+               |;| ...
+               (~and rhs-forms (~not =>) (~not |;|)) ...
+               |;| ...)
+         ...})
+
+     #'(match (sl:parens arg-forms ...)
+         [(sl:pattern lhs-forms ...) (sl:parens rhs-forms ...)] ...
+         )]))
+
+(define-match-expander sl:pattern
+  (syntax-parser
+    ; grouping
+    [(_ (pat ...)) #'(sl:pattern pat ...)]
+
+    ; structs
+    [(_ C:id) #:when (identifier-capitalized? #'C)
+     ; since nullary structs are singletons, just use eq
+     #'(== C eq?)]
+    [(_ C:id x ...) #:when (identifier-capitalized? #'C)
+     #'(C (sl:pattern x) ...)]
+
+    ; constants
+    [(_ (~or v:string v:boolean v:number)) #'v]
+
+    ; variable / catchall
+    [(_ x:id) #:when (not (identifier-capitalized? #'x))
+     #'x]))
+(begin-for-syntax
+  (define (identifier-capitalized? x)
+    (char-upper-case? (string-ref (symbol->string (syntax-e x)) 0))))
