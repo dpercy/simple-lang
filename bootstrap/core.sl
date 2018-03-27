cc5ba2d52aab1e45b428c96ce74ea3e5cc1a2221
David Percy <davetp425@gmail.com>
2018-03-26 22:31:42 -0400

    reset

--- bootstrap/core.sl
+++
@@ -1,4 +0,0 @@
-#lang reader "sl.rkt"
-
-struct Quote val
-struct App f x
--- bootstrap/sl.rkt
+++
@@ -1,6 +0,0 @@
-#lang s-exp syntax/module-reader
-"sl-mlang.rkt"
-#:read curly/read
-#:read-syntax curly/read-syntax
-
-(require (prefix-in curly/ "reader.rkt"))
