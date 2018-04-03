030c85c703ee957c296e672445c2e87e520f8a79
David Percy <davetp425@gmail.com>
2018-04-02 23:07:21 -0400

    Revert "compile to closures"
    This reverts commit 238c84fefd6f7320b8763f67164c919d5b9b6b48.

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function run(program) {
     const emptyArgs = [];
     const emptyEnv = {};
     const emptyK = (v => v);
-    return program.map(expr => compileExpr(expr)(emptyArgs, emptyEnv, emptyK));
+    return program.map(expr => runExpr(expr, emptyArgs, emptyEnv, emptyK));
 }
 
 /*
@@ -41,7 +41,6 @@ class Lambda {
         this.name = name;
         this.params = params;
         this.body = body;
-        this.bodyDenot = compileExpr(body);
     }
 }
 function show(v) {
@@ -54,34 +53,26 @@ function show(v) {
     }
 }
 
-function compileExpr(expr) {
+function runExpr(expr, args, env, k) {
     switch (expr.type) {
-    case "Num": {
-        const val = +expr.literal;
-        return function(args, env, k) {
-            return k(val);
-        };
-    }
-    case "App":
+    case "Num": return k(+expr.literal);
+    case "App": {
         const { func, arg } = expr;
-        const funcDenot = compileExpr(func);
-        const argDenot = compileExpr(arg);
-        return function(args, env, k) {
-            const argK = (argV) => funcDenot([argV, ...args], env, k);
-            return argDenot([], env, argK);
+        const argK = (argV) => {
+            return runExpr(func, [argV, ...args], env, k);
         };
-    case "Id":
-        return function(args, env, k) {
-            const val = lookup(expr.name, env);
-            return applyValArgs(val, args, k);
-        }
-    default:
-        throw new Error("bad expr type: " + expr.type);
+        return runExpr(arg, [], env, argK);
+    };
+    case "Id": {
+        const val = lookup(expr.name, env);
+        return applyValArgs(val, args, k);
+    }
+    default: throw new Error("bad expr type: " + expr.type);
     }
 }
 function applyValArgs(val, args, k) {
     if (args.length === 0) {
-        // just a lookup; not actually an application
+            // just a lookup; not actually an application
         return k(val);
     } else if (typeof val === 'function') {
         if (val.length > args.length) {
@@ -101,7 +92,7 @@ function applyValArgs(val, args, k) {
             for (let i=0; i<val.params.length; ++i) {
                 env[val.params[i]] = enoughArgs[i];
             }
-            return val.bodyDenot(extraArgs, env, k);
+            return runExpr(val.body, extraArgs, env, k);
         }
     } else if (val instanceof Closure) {
         return applyValArgs(val.func, val.args.concat(args), k);
